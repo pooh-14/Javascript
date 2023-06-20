5d9744dd7eabd9808790def59166a0ff053445a0
pooh-14 <poojavetal14@gmail.com>
2023-06-20 11:36:14 +0530

    done

--- Day30-20-06.js
+++ Day30-20-06.js
@@ -1,40 +1,41 @@
+
 var array = [{ id: 1, name: "Rahul", age: 23, surname: "xyz" },
 { id: 2, name: "Rakesh", age: 24, surname: "xyz" },
 { id: 3, name: "Swaraj", age: 25, surname: "xyz" },
 { id: 4, name: 'abc', age: 29, surname: "xyz" }];
 
-// var userId = 2;
-// var selecteduser = array.find((user)=> user.id==userId)
-// console.log(selecteduser)
+var userId = 2;
+var selecteduser = array.find((user)=> user.id==userId)
+console.log(selecteduser)
 
-// var userAge = 25;
-// var selecteduser = array.find((object)=> object.age==userAge)
-// console.log(selecteduser)
+var userAge = 25;
+var selecteduser = array.find((object)=> object.age==userAge)
+console.log(selecteduser)
 
-// var userName= "Rahul";
-// var selecteduser = array.find((user)=> user.name== userName)
-// console.log(selecteduser)
+var userName= "Rahul";
+var selecteduser = array.find((user)=> user.name== userName)
+console.log(selecteduser)
 
-// var userName = 'Rakesh';
-// var index = array.findIndex((subject) => subject.name == userName);
-// console.log(index)
+var userName = 'Rakesh';
+var index = array.findIndex((subject) => subject.name == userName);
+console.log(index)
 
-// var userName = 'giuhouhl';
-// var index = array.findIndex((subject) => subject.name == userName);
-// console.log(index)
+var userName = 'giuhouhl';
+var index = array.findIndex((subject) => subject.name == userName);
+console.log(index)
 
-// array.map((object)=>{
-//     console.log(object)
-// })
+array.map((object)=>{
+    console.log(object)
+})
 
-// var numbers = [1, 8, 9, 15, 3, 7, 37, 2, 6, 1, 6, 7, 9];
-// console.log(numbers.sort())
+var numbers = [1, 8, 9, 15, 3, 7, 37, 2, 6, 1, 6, 7, 9];
+console.log(numbers.sort())
 
-// var numbers = [1, 8, 9, 15, 3, 7, 37, 2, 6, 1, 6, 7, 9];
-// console.log(numbers.sort((a,b)=>a-b))
+var numbers = [1, 8, 9, 15, 3, 7, 37, 2, 6, 1, 6, 7, 9];
+console.log(numbers.sort((a,b)=>a-b))
 
-// var numbers = [1, 8, 9, 15, 3, 7, 37, 2, 6, 1, 6, 7, 9];
-// console.log(numbers.sort((a,b)=>b-a))
+var numbers = [1, 8, 9, 15, 3, 7, 37, 2, 6, 1, 6, 7, 9];
+console.log(numbers.sort((a,b)=>b-a))
 
 const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 const result = words.filter(word => word.length <10);
