c99258514be0caea72a1b83d701334127606b60f
pooh-14 <poojavetal14@gmail.com>
2023-06-23 23:04:48 +0530

    done

--- leetcode.js
+++ leetcode.js
@@ -1,15 +1,15 @@
 // Q 27. Remove Element
 
-var res = []
-var nums = [3,2,2,3];
-var val = 3
+// var res = []
+// var nums = [3,2,2,3];
+// var val = 3
 
-for(let i=0 ; i<nums.length; i++){
-    if(nums[i] !== val){
-        res.push(nums[i])
-    }
-}
-console.log(res);
+// for(let i=0 ; i<nums.length; i++){
+//     if(nums[i] !== val){
+//         res.push(nums[i])
+//     }
+// }
+// console.log(res);
   
 
 var res = []
