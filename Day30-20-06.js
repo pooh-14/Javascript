6c9fedae299ec9954efa1a1aca7085396338990e
pooh-14 <poojavetal14@gmail.com>
2023-06-29 22:56:18 +0530

    done

--- Day30-20-06.js
+++ Day30-20-06.js
@@ -1,8 +1,8 @@
 
-// var array = [{ id: 1, name: "Rahul", age: 23, surname: "xyz" },
-// { id: 2, name: "Rakesh", age: 24, surname: "xyz" },
-// { id: 3, name: "Swaraj", age: 25, surname: "xyz" },
-// { id: 4, name: 'abc', age: 29, surname: "xyz" }];
+var array = [{ id: 1, name: "Rahul", age: 23, surname: "xyz" },
+{ id: 2, name: "Rakesh", age: 24, surname: "xyz" },
+{ id: 3, name: "Swaraj", age: 25, surname: "xyz" },
+{ id: 4, name: 'abc', age: 29, surname: "xyz" }];
 
 // var userId = 2;
 // var selecteduser = array.find((user)=> user.id==userId)
@@ -24,9 +24,9 @@
 // var index = array.findIndex((subject) => subject.name == userName);
 // console.log(index)
 
-// array.map((object)=>{
-//     console.log(object)
-// })
+array.map((object)=>{
+    console.log(object)
+})
 
 // var numbers = [1, 8, 9, 15, 3, 7, 37, 2, 6, 1, 6, 7, 9];
 // console.log(numbers.sort())
